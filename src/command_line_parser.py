af4c1e2f4bdf4494af0cca55f7cbd36f926bf129
thiran <antoth.web@outlook.fr>
2024-12-08 14:50:28 +0100

    Vérification que l'argument "part" est bien un entier

--- src/command_line_parser.py
+++ src/command_line_parser.py
@@ -13,11 +13,15 @@ def get_arguments_from_command_line() -> tuple[str, int]:
                         Each problem has exactly 2 parts")
     args = parser.parse_args()
     filename = args.filename
-    part = int(args.part)
+    part = args.part
     # vérifier que le nom du fichier se termine par ".txt"
     if not filename.endswith(".txt"):
         raise ValueError("Filename MUST be a txt file")
+    # vérifier que part est bien un entier
+    if not part.isdecimal():
+        raise ValueError("Argument part MUST be an integer")
+    part = int(part)
     # vérifier que le numéro de la partie est 1 ou 2
     if part not in (1, 2):
-        raise ValueError("Part argument (--part) MUST be 1 or 2")
+        raise ValueError("Argument part MUST be 1 or 2")
     return filename, part
