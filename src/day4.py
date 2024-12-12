57fb4f4e0c17e726c6cd6d6591d26998d5e43911
antoinethirion <antoine.thirion@wavestone.com>
2024-12-12 21:32:09 +0100

    Two parts of day 4 completed

--- src/day4.py
+++ src/day4.py
@@ -12,8 +12,9 @@ def remove_incorrect_positions(times):
 ################################## PART 1 ##################################
 
 
-def search_from_position(grid: list[list[str]], position: tuple[int, int], keyword: str) -> int:
-  """Cherche le nombre d'occurences du keyword à partir de la position donnée
+def search_from_position(grid: list[list[str]], start_position: tuple[int, int], keyword: str) -> int:
+  """Cherche le nombre d'occurences du keyword à partir de la position donnée. A la position initiale,
+  on a forcément la première lettre du keyword, à savoir 'X' 
   On peut avoir au plus 8 occurences (keyword à l'endroit, à l'envers, à l'horizontale,
   à la verticale ou en diagonale)
   :param grid: grille de recherche
@@ -28,7 +29,7 @@ def search_from_position(grid: list[list[str]], position: tuple[int, int], keywo
                 (-1, -1), (1, 1),     # diagonale descendante
                 (1, -1), (-1, 1),     # diagonale montante
                 ]
-  start_line, start_column = position
+  start_line, start_column = start_position
   for step_row, step_column in directions:
     for keyword_index in range(1, keyword_length):
       new_line = start_line + step_row * keyword_index
@@ -45,9 +46,9 @@ def search_from_position(grid: list[list[str]], position: tuple[int, int], keywo
     if (keyword_index == keyword_length - 1 and
             current_letter == keyword[keyword_index]):
       xmas_local_count += 1
-    if xmas_local_count:
-      # au moins une occurence de xmas a été trouvée
-      match_positions.append(position)
+  if xmas_local_count:
+    # au moins une occurence de keyword a été trouvée
+    match_positions.append(start_position)
   return xmas_local_count
 
 
@@ -64,36 +65,50 @@ def count_xmas_occurrences(grid: list[list[str]], keyword: str) -> int:
 ################################## PART 2 ##################################
 
 
-def cross_search_from_position(grid: list[list[str]], position: tuple[int, int], keyword: str) -> int:
-  """Cherche le nombre d'occurences du keyword en forme de croix à partir de la position donnée
+def found_cross_pattern_from_position(grid: list[list[str]], start_position: tuple[int, int], keyword: str) -> bool:
+  """Indique si le pattern MAS en forme de croix est présent à la position donnée.
+  A la position initiale, on a forcément la lettre 'A'
   :param grid: grille de recherche
   :param position : position à partir de laquelle chercher le keyword
   :param keyword : le keyword à chercher
   :returns nombre d'occurences du keyword trouvées
   """
-  xmas_local_count = 0
-  keyword_length = len(keyword)
-  directions = [(-1, -1), (1, 1),     # diagonale descendante
-                (1, -1), (-1, 1),     # diagonale montante
-                ]
-  start_line, start_column = position
-  for step_row, step_column in directions:
-    for keyword_index in range(1, keyword_length):
-      new_line = start_line + step_row * keyword_index
-      new_column = start_column + step_column * keyword_index
-      if 0 <= new_line < len(grid) and 0 <= new_column < len(grid[0]):
-        current_letter: str = grid[new_line][new_column]
-        match_positions.append((new_line, new_column))
-        if current_letter != keyword[keyword_index]:
-          remove_incorrect_positions(keyword_index)
-          break
-      else:
-        remove_incorrect_positions(keyword_index)
-        break
-    if (keyword_index == keyword_length - 1 and
-            current_letter == keyword[keyword_index]):
-      xmas_local_count += 1
-  return xmas_local_count
+  pattern_found = False
+  row, column = start_position
+  # diagonale descendante
+  top_left, center, bottom_right = (-1, -1), (0, 0), (1, 1)
+  bottom_left, center, top_right = (1, -1), (0, 0), (-1, 1)
+  descending_diagonale = ""
+  # on concatène les éléments de la grille de la diagonale descendante dans la variable descending_diagonale
+  # cette variable aura pour valeur soit "MAS" soit "SAM" dans le cas où un pattern est trouvé
+  for position in (top_left, center, bottom_right):
+    step_row, step_column = position
+    new_row, new_column = row + step_row, column + step_column
+    if not (0 <= new_row < len(grid) and 0 <= new_column < len(grid[0])):
+      return False
+    descending_diagonale += grid[new_row][new_column]
+  ascending_diagonale = ""
+  # on concatène les éléments de la grille de la diagonale montante dans la variable ascending_diagonale
+  # cette variable aura pour valeur soit "MAS" soit "SAM" dans le cas où un pattern est trouvé
+  for position in (bottom_left, center, top_right):
+    step_row, step_column = position
+    new_row, new_column = row + step_row, column + step_column
+    if not (0 <= new_row < len(grid) and 0 <= new_column < len(grid[0])):
+      return False
+    ascending_diagonale += grid[new_row][new_column]
+  # vérifier que la diagonale descendante correspond à "MAS" ou "SAM"
+  correct_descending_diagonale = (descending_diagonale == keyword or
+                                  descending_diagonale == keyword[::-1])
+  # vérifier que la diagonale montante correspond à "MAS" ou "SAM"
+  correct_ascending_diagonale = (ascending_diagonale == keyword or
+                                 ascending_diagonale == keyword[::-1])
+  if sum((correct_descending_diagonale, correct_ascending_diagonale)) == 2:
+    pattern_found = True
+    # ajouter les positions du pattern
+    pattern_positions: list[tuple[int, int]] = [(row + position[0], column + position[1])
+                                                for position in (top_left, center, bottom_right, bottom_left, top_right)]
+    match_positions.extend(pattern_positions)
+  return pattern_found
 
 
 def count_crossed_mas_occurrences(grid: list[list[str]], keyword: str) -> int:
@@ -101,11 +116,12 @@ def count_crossed_mas_occurrences(grid: list[list[str]], keyword: str) -> int:
   max_lines, max_columns = len(grid), len(grid[0])
   for line in range(max_lines):
     for column in range(max_columns):
-      if grid[line][column] == keyword[0]:
+      if grid[line][column] == 'A':
         position = (line, column)
-        match_positions.append(position)
-        xmas_count += cross_search_from_position(grid, position, keyword)
+        if found_cross_pattern_from_position(grid, position, keyword):
+          xmas_count += 1
   return xmas_count
+
 
 ############################## LAUNCH PROGRAM ##############################
 
@@ -155,5 +171,6 @@ if __name__ == "__main__":
     keyword = "MAS"
     print("Number of times crossed MAS appears:",
           count_crossed_mas_occurrences(grid, keyword))
-  new_grid = build_grid_with_match_positions(grid)
-  display_grid(new_grid)
+  # ne pas afficher la liste lorsque l'input est trop grand
+  # new_grid = build_grid_with_match_positions(grid)
+  # display_grid(new_grid)
